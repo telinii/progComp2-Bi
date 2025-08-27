a72221a8f4b8c66153423cf8704413ac7ff81c09
Luca Telini Crozara <gply714@gmail.com>
2025-08-27 20:32:31 -0300

    Add files via upload
    funções de String

---
+++ funçõeszinhas.js
@@ -0,0 +1,26 @@
+//funçõeszinhas string
+
+let frase = "estou estudandando JS"
+
+//insirindo "frase" no console , mostra a string,
+//insirindo frase[x] = mostra o caractere da posição
+
+//frase.length = mostra a quantidade de caractere
+
+//frase.toUpperCase()
+//'ESTOU ESTUDANDANDO JS' -> letra maiuscula
+
+//frase.toLowerCase()
+//'estou estudandando js' -> letra minuscula
+
+//frase.toUpperCase().includes(`js`)
+//false
+//frase.toUpperCase().includes(`JS`)
+//true
+
+//frase.slice(0,5)
+//'estou'
+//frase.slice(0,9)
+//'estou est'
+//frase.slice(-5)
+//'do JS'
